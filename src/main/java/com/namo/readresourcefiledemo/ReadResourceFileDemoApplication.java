3e56e7725d62edf73b269be9d3c75cae77bf21c8
wx1458084829 <15328400570@163.com>
2022-06-08 09:20:32 +0800

    init project

---
+++ src/main/java/com/namo/readresourcefiledemo/ReadResourceFileDemoApplication.java
@@ -0,0 +1,13 @@
+package com.namo.readresourcefiledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadResourceFileDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadResourceFileDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/namo/readresourcefiledemo/ReadResourceFileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namo.readresourcefiledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadResourceFileDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
